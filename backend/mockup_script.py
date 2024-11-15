3947e62b6533130a86d3473e1886450bb5279996
Emily <137836817+Emily-D-2003@users.noreply.github.com>
2024-11-15 00:07:53 -0500

    front-back-end integrated

--- backend/mockup_script.py
+++ backend/mockup_script.py
@@ -1,8 +1,23 @@
 from flask import Flask, jsonify, request 
 from flask_cors import CORS
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import dev.spotAPI_base
+import dev.spotAPI_minehelper
+import dev.spotAPI_minedata
+import dev.spotAPI_recSystem
+from dev.spotAPI_recSystem import get_recs
 
 app = Flask(__name__, static_folder='static')
 cors = CORS(app, origins='*')
+
+# Spotify API setup (assuming credentials are in environment variables)
+client_id = os.getenv('SPOTIFY_CLIENT_ID')
+client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
 #this is an example to see how a python script would send data to the frontend
 #if you are trying to run this, make sure to use a virtual environment! :) 
 #note: the following commands are for windows
@@ -15,50 +30,20 @@ cors = CORS(app, origins='*')
 def get_songs():
     song_name = request.args.get('song_name')
 
+    if not song_name:
+        return jsonify({"error": "Please provide a song name"}), 400
+
+    result = get_recs(song_name)
+    
     return jsonify(
         {
             "songs": [
                 {
-                    "song_name": "Evil Lisa's Song",
-                    "artist": "Lisa",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                {
-                    "song_name": "Evil Tony's Song",
-                    "artist": "Tony",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                {
-                    "song_name": "Evil Emily's Song",
-                    "artist": "Emily",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                {
-                    "song_name": "Evil Marlea's Song",
-                    "artist": "Marlea",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                                {
-                    "song_name": "Lisa's Song",
-                    "artist": "Lisa",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                {
-                    "song_name": "Tony's Song",
-                    "artist": "Tony",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                {
-                    "song_name": "Emily's Song",
-                    "artist": "Emily",
-                    "album_cover": "/static/assets/sample_background.png"
-                },
-                {
-                "song_name": "Evil Marlea's Song",
-                "artist": "Marlea",
-                "album_cover": "/static/assets/sample_background.png"
+                    "song_name": row["Track Name"],
+                    "artist": row["Artist"],
+                    "album_cover": row["Thumbnail URL"]
                 }
-
+                for _, row in result.iterrows()
             ]
         }
     )
